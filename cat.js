db8bf466c901fce8467ee87dc947c6ad38c95d52
lifetea <494886251@qq.com>
2014-12-21 10:35:15 +0000

    update

--- cat.js
+++ cat.js
@@ -1,5 +1,4 @@
 var config = require("./config.json");
-var http 	 = require("http");
 var request = require('request');
 var url      = require("url");
 var fs       = require('fs');
@@ -9,23 +8,29 @@ var  mongodb = require('mongodb');
 var  server  = new mongodb.Server('localhost', 27017, {auto_reconnect:true});
 var  db 	 = new mongodb.Db('xs', server, {safe:true});
 var links ="",collect=null,count=0;
-var template = "",eles =[]; 
+var template = "",eles =[],news=[]; 
+var getfn = function(u){
+	return (url.parse(u["attribs"].href).pathname).slice(1,-1);
+};
 
 var update =function(){
 	async.waterfall([
 		 function(cb){
+//			db.close();
 			fs.exists('./wanmei/index.htm', function (exists) {
 			  var filepath = !!exists ? './wanmei/index.htm' : './tp/cat.htm';
 			  fs.readFile(filepath,cb);
 			});
 		 },
          function(temp,cb){
+			 console.log("444");
 			 template = temp.toString();
 			 count = $("#container a",template).length;
 			 request(config.href, cb);
          },
 		 function(response,body,cb){
 		  	if(response.statusCode == 200){
+		  		console.log("status 200");
 		   		links = $("a",$(".cat_post",body)[0]);
 		   		db.open(cb);
 		  	}else{
@@ -78,13 +83,19 @@ var update =function(){
 			 }
 		 },
 	    function(cb){
-	        var chunks = [];
+	        var chunks = [],newstr="";
 	        for(var i = 0,len =eles.length; i<len;i++){
 	            var content = "<li><a href='"+ eles[i].filename +"'>"+eles[i].title+"</a></li>";
 	            chunks.push(content);
 	        }
+	   		for(var len=links.length,k=links.length-4;k < len;k++){
+	   			var fn = (url.parse(links[k]["attribs"].href).pathname).slice(1,-1);
+	   			var title = links[k]["attribs"].title;
+	   			newstr += "<li><a href='"+ fn +"'>"+title+"</a></li>";
+	   		}
 	        var html = $.load(template);
 	        html("#container").append(chunks.join(""));
+	        html("#news").html(newstr);
 	        console.log("complete write");
 	        fs.writeFile('./wanmei/index.htm', html.html(),{encoding:"utf8"},cb);
 	    }

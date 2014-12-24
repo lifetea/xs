eff3683f54307d1f1bc918c362cf88a72946b2d3
lifetea <494886251@qq.com>
2014-12-24 13:12:01 +0000

    update

--- content.js
+++ content.js
@@ -27,7 +27,7 @@ var run =function(){
 	        collection.find({"flag":0}).sort({"_id":1}).toArray(cb);
 	    },
 	    function(docs,cb){
-	    	if(!!dob.rel){
+	    	if(!!ob&&!!ob.rel){
 	    		cb(true);
 	    	}else{
 	    		ob = docs[0];

3ae713c5c828cc3919558a701492622c6a44bc25
lifetea <494886251@qq.com>
2014-12-24 11:03:14 +0000

    优化

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var schedule = require('node-schedule');
 var catrule = new schedule.RecurrenceRule();
 
 catrule.minutes = [5,25];
-catrule.hour = [0,6,22,23];
+catrule.hour = [0,6,12,21,22,23];
 
 var j = schedule.scheduleJob(catrule, function(){
 	cat.update();
 });
 
 var contrule = new schedule.RecurrenceRule();
-contrule.hour = [0,6,22,23];
+contrule.hour = [0,6,12,21,22,23];
 contrule.minutes = [20,55];
 
 var k = schedule.scheduleJob(contrule, function(){

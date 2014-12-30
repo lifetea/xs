9df11c73826965f8003340b6f7db095abfc46b0f
lifetea <494886251@qq.com>
2014-12-30 11:52:35 +0800

    update

--- wanmei.js
+++ wanmei.js
@@ -3,13 +3,13 @@ var schedule = require('node-schedule');
 
 
 var capt = new schedule.RecurrenceRule();
-capt.minutes = [0,30];
+capt.minute = [0,30];
 var j = schedule.scheduleJob(capt, function(){
 	nov.capt();
 });
 
 var cont = new schedule.RecurrenceRule();
-cont.minutes = [15,45];
+cont.minute = [15,45];
 var k = schedule.scheduleJob(cont, function(){
 	nov.cont();
 });
